0b29f0d2b08133e07f764acc78bee464c77c366a
Vladimir <valozovoy@edu.hse.ru>
2021-04-26 02:06:35 +0300

    added plots to model.R

--- scripts/Models.R
+++ scripts/Models.R
@@ -3,10 +3,12 @@ library(tidyverse) # работа с табличками
 library(lubridate) # работа с датами
 library(broom) # обработка табличек с результатами
 library(ggplot2) # графики
+library(gridExtra) # несколько графиков в одном
 library(urca) # работа с временными рядами
 library(forecast) # работа с временными рядами
 library(rugarch) # одномерные гарчи
 library(rmgarch) # многомерные гарчи
+
 
 # загрузка данных
 dfpath <- str_replace(getwd(), '/scripts', '/data/returns.csv')
@@ -15,6 +17,26 @@ df <- read.csv(dfpath)
 df$TRADEDATE <- as.Date(df$TRADEDATE, format = '%Y-%m-%d')
 glimpse(df)
 # отлично, все данные открываются, причём цифры в нужном формате
+
+# посмотрим на распределения доходностей и сравним их с нормальными
+compare_distribution <-
+  function(x, name) {
+    plot <- qplot(x, geom = 'density',
+                  main = paste0('Распределение ', toString(name), ' (синее) и нормальное')) +
+      geom_density(fill = 'blue', alpha = 0.4) +
+      geom_density(aes(rnorm(200000, mean(x), sd(x))), fill = 'red', alpha = 0.25) +
+      labs(x = '') +
+      theme(plot.title = element_text(hjust = 0.5))
+
+    return(plot)
+  }
+
+grid.arrange(compare_distribution(df[!is.na(df$FIVE), 'FIVE'], 'FIVE'),
+             compare_distribution(df$'MGNT', 'MGNT'),
+             compare_distribution(df$'LNTA', 'LNTA'),
+             compare_distribution(df$'MVID', 'MVID'),
+             ncol = 2)
+
 
 #####################
 # Univariate models #
@@ -42,7 +64,7 @@ tsdisplay((df$MVID^2), main = 'MVID squared returns')
 # https://www.rdocumentation.org/packages/rugarch/versions/1.4-4/topics/ugarchspec-methods
 # в модели среднего мы предполагаем, что доходность M.Video около нуля и константа не нужна
 mvid_spec <- ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
-                        mean.model = list(armaOrder = c(2, 1), include.mean = FALSE))
+                        mean.model = list(armaOrder = c(3, 1), include.mean = FALSE))
 mvid_model <- ugarchfit(spec = mvid_spec, data = df$MVID)
 # посмотрим на результаты оценивания
 mvid_model
@@ -57,7 +79,7 @@ plot(mvid_model, which = 11)
 # теперь попробуем учесть в модели не-Гауссово распределение доходностей
 # у нас есть альтернатива - в R реализовано, например, распределение Стьюдента для случайной ошибки в GARCH
 mvid_spec_t <- ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1, 1)),
-                          mean.model = list(armaOrder = c(2, 1), include.mean = FALSE),
+                          mean.model = list(armaOrder = c(3, 1), include.mean = FALSE),
                           distribution.model = 'std')
 mvid_model_t <- ugarchfit(spec = mvid_spec_t, data = df$MVID)
 # посмотрим на результаты оценивания
